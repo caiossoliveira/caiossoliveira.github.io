1f8cf40cc2b3c11faa4fa208cce6327e81ec88c0
Anoni <caiossoliveira@gmail.com>
2020-06-29 21:31:48 -0300

    some improvements

--- js/index.js
+++ js/index.js
@@ -1,9 +1,25 @@
-function selectCurriculum(){
-  document.getElementById("currilucumLanguages").style.display = "block";
+function selectResume(){
+  if(isFirstClick()){
+  	document.getElementById("currilucumLanguages").style.display = "block";
+  }
+  else{
+  	toClose();
+  }
 }
 
 function toClose(){
 	document.getElementById("currilucumLanguages").style.display = "none";
+}
+
+function isFirstClick(){
+	if(typeof isFirstClick.isFirstClick == 'undefined'){
+		isFirstClick.isFirstClick = true;
+	}
+	else{
+		isFirstClick.isFirstClick = !isFirstClick.isFirstClick;
+	}
+
+	return isFirstClick.isFirstClick;
 }
 
 function toAlert(){
