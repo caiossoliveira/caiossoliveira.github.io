d48e704ed1dc1229d13d739003bbd20eb1832fbc
Anoni <caiossoliveira@gmail.com>
2020-07-12 20:57:04 -0300

    portfolio added

--- js/index.js
+++ js/index.js
@@ -34,3 +34,36 @@ function isFirstClick(){
 	return isFirstClick.isFirstClick;
 }
 
+//portfolio
+$(document).ready(function(){
+  $('.carousel').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    dots:true,
+    arrows: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+});
+
+src="https://code.jquery.com/jquery-1.11.0.min.js"
+src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"
